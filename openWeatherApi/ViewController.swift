65ca580586156088ff87516311485b97b55a1644
edo lorenza <edolrnz@gmail.com>
2020-08-06 11:25:37 +0700

    Initial Commit

---
+++ openWeatherApi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  openWeatherApi
+//
+//  Created by Edo Lorenza on 06/08/20.
+//  Copyright © 2020 Edo Lorenza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
